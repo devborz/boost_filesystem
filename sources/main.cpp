1a8e8d7497a0bc7aef1d0bd22cf7c0edcec4b92c
devborz <u.turkaev@yandex.ru>
2019-11-11 17:49:58 +0300

    11.11.2019 #2

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,14 +1,14 @@
 #include "filesys.h"
 
 int main(int argc, char *argv[]) {
-    /*if (argc < 2)
+    if (argc < 2)
     {
-      cout << "Usage:  path\n";
-      return 1;
+        path path_to_ftp("ftp");
     }
-    */
-    //path path_to_ftp("argv[1]");
-    path path_to_ftp("files");
+    else {
+        path path_to_ftp(argv[1]);
+    }
+
 
     iterate(path_to_ftp);
 
