690c4411bda954337300b80ea4b65cdf48f86de0
devborz <u.turkaev@yandex.ru>
2019-11-11 17:53:11 +0300

    11.11.2019 #3

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,13 +4,15 @@ int main(int argc, char *argv[]) {
     if (argc < 2)
     {
         path path_to_ftp("ftp");
+        iterate(path_to_ftp);
     }
     else {
         path path_to_ftp(argv[1]);
+        iterate(path_to_ftp);
     }
 
 
-    iterate(path_to_ftp);
+
 
     return 0;
 }
